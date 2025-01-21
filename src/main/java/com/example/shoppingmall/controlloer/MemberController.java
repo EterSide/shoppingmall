41bb814bb15e2dbbb76f0a2b3e7a4268d29ea3fd
EterSide <138553106+EterSide@users.noreply.github.com>
2025-01-21 21:05:47 +0900

    MemberController 완료 - 회원 가입 (GET,POST)

--- src/main/java/com/example/shoppingmall/controlloer/MemberController.java
+++ src/main/java/com/example/shoppingmall/controlloer/MemberController.java
@@ -1,17 +1,32 @@
 package com.example.shoppingmall.controlloer;
 
+import com.example.shoppingmall.entitiy.Member;
 import com.example.shoppingmall.repository.MemberRepository;
+import com.example.shoppingmall.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class MemberController {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public String addMember() {
-        return "member";
+    @GetMapping("/signup")
+    public String signUp(Model model) {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signUp(Member member) {
+        memberService.save(member);
+        return "redirect:/login";
     }
 
 }
+
